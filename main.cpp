1b3a1401227a7e8c0729a501b1fd1181d4837dd0
mikromavitsus <mikkokuha@hotmail.com>
2016-09-21 18:45:07 +0300

    Imported data from LHAPDF library, made F_SES function, integral limits and variables need to be made to make sense.

--- main.cpp
+++ main.cpp
@@ -4,12 +4,17 @@
 #include "cubature.h"
 #include <gsl/gsl_math.h>
 
+#include "LHAPDF/LHAPDF.h"
+
 
 using namespace std;
+using namespace LHAPDF;
 
 ///GLOBALS///
 
 const unsigned g_dim = 3;
+const string g_pdfsetname = "HERAPDF20_LO_EIG";
+const int g_pdfsetmember = 1;
 
 
 ///PROTOTYPES///
@@ -17,10 +22,20 @@ const unsigned g_dim = 3;
 int integrand_function(unsigned, const double*, void*, unsigned, double*);
 int phasespace_integral(const double[], const double[], double * const, double * const);
 int calculate_sigma_jet(double * const, double * const);
-
+double f_ses(const double * const, const double * const, const PDF*);
 
 
 ///FUNCTIONS///
+
+double f_ses(const double * const p_x, const double * const p_q2, const PDF* p_pdf){
+    double sum = 0;
+    for (int flavour=-6; flavour<=6; ++flavour){
+        sum += (flavour == 0) ? p_pdf->xfxQ2(flavour, *p_x, *p_q2) : 4 * p_pdf->xfxQ2(flavour, *p_x, *p_q2) / 9;
+    }
+    //cout<<"x="<<*p_x<<", q2="<<*p_q2<<" SES="<<sum<<endl;
+    return sum;
+}
+
 
 int integrand_function(unsigned ndim,     //Variable dimension
                        const double *p_x, //Pointer to variable
@@ -28,12 +43,14 @@ int integrand_function(unsigned ndim,     //Variable dimension
                        unsigned fdim,     //Output dimension
                        double *p_fval) {  //Pointer to output
 
-    const double sigma = *((double *) p_fdata);
-    double sum = 0;
-    unsigned i;
+    const PDF* p_pdf = (PDF *) p_fdata;
+    const double kt2 = p_x[0], y1 = p_x[1], y2 = p_x[2];
 
-    for (i = 0; i < ndim; ++i) sum += p_x[i] * p_x[i];
-    p_fval[0] = exp(-sigma * sum);
+    double subprocess_cs = 7;
+
+    p_fval[0] = 0.5 * y1 * f_ses(&y1, &kt2, p_pdf)
+                    * y2 * f_ses(&y2, &kt2, p_pdf)
+                    * subprocess_cs;
 
     return 0; // success
 }
@@ -44,11 +61,11 @@ int phasespace_integral(const double upper_limits [3],
                         double * const p_error){
 
     const unsigned fdim=1;
-    double fdata= 0.5;
+    PDF* p_pdf = mkPDF(g_pdfsetname, g_pdfsetmember);
 
     int not_success = pcubature(fdim, //Integrand dimension
               integrand_function, //Integrand function
-              &fdata,              //Pointer to additional arguments
+              p_pdf,              //Pointer to additional arguments
               g_dim,              //Variable dimension
               lower_limits,       //Variables minimum
               upper_limits,       //Variables maximum
@@ -68,7 +85,7 @@ int phasespace_integral(const double upper_limits [3],
 
 int calculate_sigma_jet(double * const p_value, double * const p_error){
     int not_success;
-    const double upper_limits [3] = {2,2,2}, lower_limits [3] = {-2,-2,-2};
+    const double upper_limits [3] = {10,1,1}, lower_limits [3] = {1,1E-5,1E-5};
 
     not_success = phasespace_integral(upper_limits, lower_limits, p_value, p_error);
     return not_success;
@@ -81,6 +98,8 @@ int main()
 {
     int not_success;
     double sigma_jet, error;
+
+
 
     not_success = calculate_sigma_jet(&sigma_jet, &error);
 
