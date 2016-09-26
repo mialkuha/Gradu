865b2cc7162ca27dc4c315df4c124b9e4c3cd2ef
mikromavitsus <mikkokuha@hotmail.com>
2016-09-26 11:28:29 +0300

    Reordering of the code or better readability. No actual changes made.

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ const int g_pdfsetmember = 1;                    //Member ID of the pdf in the s
 const double g_s = 10;                           //Mandelstam variable s
 const double g_kt_upper_cutoff = 10;             //Upper cutoff kt used in integration
 const double g_kt_lower_cutoff = 1;              //Lower cutoff kt used in integration
-const double g_error_tolerance = 1e-6;           //Global error tolerance
+const double g_error_tolerance = 1e-4;           //Global error tolerance
 
 
 
@@ -70,79 +70,64 @@ int main()
 
 
 
-///sigma_gg_gg
-///Calculates and returns the cross section of the subprocess gg->gg. Return value should be real.
+///calculate_sigma_jet
+///Function that does the calculation of the sigma_jet. Calls phasespace_integral.
 ///
-///\param p_s_hat = pointer to subprocess mandelstam variable s
-///\param p_t_hat = pointer to subprocess mandelstam variable t
-///\param p_u_hat = pointer to subprocess mandelstam variable u
+///\param p_value = pointer to the destination of the value of the sigma_jet
+///\param p_error = pointer to the destination of the error output from cubature
 ///
-complex<double> sigma_gg_gg(const complex<double> * const p_s_hat, const complex<double> * const p_t_hat, const complex<double> * const p_u_hat)
+///\return 0 on successful run
+///
+int calculate_sigma_jet(double * const p_value, double * const p_error)
 {
-    const complex<double> s = *p_s_hat, t = *p_t_hat, u = *p_u_hat, constant = 3;
-    return 4.5*(constant-(u*t)/(s*s)-(u*s)/(t*t)-(s*t)/(u*u));
+    int not_success;
+    const double upper_limits [3] = {g_kt_upper_cutoff, 1, 1};
+    const double lower_limits [3] = {g_kt_lower_cutoff, 0, 0};
+
+    not_success = phasespace_integral(upper_limits, lower_limits, p_value, p_error);
+    return not_success;
 }
 
 
-///u_hat
-///Calculates and returns the subprocess mandelstam variable u. Return value can be complex.
+///phasespace_integral
+///Function that calculates the phasespace integral with cubature algorithm. Uses integrand_function.
 ///
-///\param p_x1 = pointer to momentum fraction of the first particle
-///\param p_x2 = pointer to momentum fraction of the other particle
-///\param p_kt2 = pointer to the transverse momentum squared
+///\param upper_limits = list of the upper limits of the integral. Order: kt2, x1, x2
+///\param lower_limits = list of the lower limits of the integral. Order: kt2, x1, x2
+///\param p_value = pointer to the destination of the value of the integral
+///\param p_error = pointer to the destination of the error output from cubature
 ///
-complex<double> u_hat(const double * const p_x1, const double * const p_x2, const double * const p_kt2)
+///\return 0 on successful run
+///
+int phasespace_integral(const double upper_limits [3], const double lower_limits [3], double * const p_value, double * const p_error)
 {
-    const double x1 = *p_x1, x2 = *p_x2, kt2 = *p_kt2;
-    const complex<double> dummy = x1*x2*(x1*x2 -4*(kt2/g_s));
-    return -0.5*g_s*(sqrt(dummy) +x1*x2);
-}
 
+    const unsigned fdim=1;
+    PDF* p_pdf = mkPDF(g_pdfsetname, g_pdfsetmember);
 
-///t_hat
-///Calculates and returns the subprocess mandelstam variable t. Return value can be complex.
-///
-///\param p_x1 = pointer to momentum fraction of the first particle
-///\param p_x2 = pointer to momentum fraction of the other particle
-///\param p_kt2 = pointer to the transverse momentum squared
-///
-complex<double> t_hat(const double * const p_x1, const double * const p_x2, const double * const p_kt2)
-{
-    const double x1 = *p_x1, x2 = *p_x2, kt2 = *p_kt2;
-    const complex<double> dummy = x1*x2*(x1*x2 -4*(kt2/g_s));
-    return 0.5*g_s*(sqrt(dummy) -x1*x2);
-}
+    int not_success = hcubature(fdim,               //Integrand dimension
+                                integrand_function, //Integrand function
+                                p_pdf,              //Pointer to additional arguments
+                                g_dim,              //Variable dimension
+                                lower_limits,       //Variables minimum
+                                upper_limits,       //Variables maximum
+                                0,                  //Max n:o of function evaluations
+                                0,                  //Required absolute error
+                                g_error_tolerance,  //Required relative error
+                                ERROR_INDIVIDUAL,   //Enumerate of which norm is used on errors
+                                p_value,            //Pointer to output
+                                p_error);           //Pointer to error output
 
-
-///s_hat
-///Calculates and returns the subprocess mandelstam variable s. Return value can be complex.
-///
-///\param p_x1 = pointer to momentum fraction of the first particle
-///\param p_x2 = pointer to momentum fraction of the other particle
-///
-complex<double> s_hat(const double * const p_x1, const double * const p_x2)
-{
-    return *p_x1 * *p_x2 * g_s;
-}
-
-
-///f_ses
-///Calculates and returns the parton distribution function F_ses according to single effective
-///subprocess approximation.
-///
-///\param p_x = pointer to momentum fraction of the particle in question
-///\param p_q2 = pointer to momentum exchange squared
-///\param p_pdf = pointer to a member of the PDF class from LHAPDF
-///
-double f_ses(const double * const p_x, const double * const p_q2, const PDF* p_pdf)
-{
-    double sum = 0;
-    for (int flavour=-6; flavour<=6; ++flavour)
+    if(not_success)
     {
-        sum += (flavour == 0) ? p_pdf->xfxQ2(flavour, *p_x, *p_q2) : 4 * p_pdf->xfxQ2(flavour, *p_x, *p_q2) / 9;
+        cout<<"Problem with integration"<<endl;
+        return 1;
     }
-    //cout<<"x="<<*p_x<<", q2="<<*p_q2<<" SES="<<sum<<endl;
-    return sum;
+    else
+    {
+        printf("Computed integral = %0.10g +/- %g\n", *p_value, *p_error);
+        return 0;
+    }
 }
 
 
@@ -180,62 +165,77 @@ int integrand_function(unsigned ndim, const double *p_x, void *p_fdata, unsigned
 }
 
 
-///phasespace_integral
-///Function that calculates the phasespace integral with cubature algorithm. Uses integrand_function.
+///s_hat
+///Calculates and returns the subprocess mandelstam variable s. Return value can be complex.
 ///
-///\param upper_limits = list of the upper limits of the integral. Order: kt2, x1, x2
-///\param lower_limits = list of the lower limits of the integral. Order: kt2, x1, x2
-///\param p_value = pointer to the destination of the value of the integral
-///\param p_error = pointer to the destination of the error output from cubature
+///\param p_x1 = pointer to momentum fraction of the first particle
+///\param p_x2 = pointer to momentum fraction of the other particle
 ///
-///\return 0 on successful run
-///
-int phasespace_integral(const double upper_limits [3], const double lower_limits [3], double * const p_value, double * const p_error)
+complex<double> s_hat(const double * const p_x1, const double * const p_x2)
 {
-
-    const unsigned fdim=1;
-    PDF* p_pdf = mkPDF(g_pdfsetname, g_pdfsetmember);
-
-    int not_success = pcubature(fdim,               //Integrand dimension
-                                integrand_function, //Integrand function
-                                p_pdf,              //Pointer to additional arguments
-                                g_dim,              //Variable dimension
-                                lower_limits,       //Variables minimum
-                                upper_limits,       //Variables maximum
-                                0,                  //Max n:o of function evaluations
-                                0,                  //Required absolute error
-                                g_error_tolerance,  //Required relative error
-                                ERROR_INDIVIDUAL,   //Enumerate of which norm is used on errors
-                                p_value,            //Pointer to output
-                                p_error);           //Pointer to error output
-
-    if(not_success)
-    {
-        cout<<"Problem with integration"<<endl;
-        return 1;
-    }
-    else
-    {
-        printf("Computed integral = %0.10g +/- %g\n", *p_value, *p_error);
-        return 0;
-    }
+    return *p_x1 * *p_x2 * g_s;
 }
 
 
-///calculate_sigma_jet
-///Function that does the calculation of the sigma_jet. Calls phasespace_integral.
+///t_hat
+///Calculates and returns the subprocess mandelstam variable t. Return value can be complex.
 ///
-///\param p_value = pointer to the destination of the value of the sigma_jet
-///\param p_error = pointer to the destination of the error output from cubature
+///\param p_x1 = pointer to momentum fraction of the first particle
+///\param p_x2 = pointer to momentum fraction of the other particle
+///\param p_kt2 = pointer to the transverse momentum squared
 ///
-///\return 0 on successful run
-///
-int calculate_sigma_jet(double * const p_value, double * const p_error)
+complex<double> t_hat(const double * const p_x1, const double * const p_x2, const double * const p_kt2)
 {
-    int not_success;
-    const double upper_limits [3] = {g_kt_upper_cutoff, 1, 1};
-    const double lower_limits [3] = {g_kt_lower_cutoff, g_error_tolerance, g_error_tolerance};
+    const double x1 = *p_x1, x2 = *p_x2, kt2 = *p_kt2;
+    const complex<double> dummy = x1*x2*(x1*x2 -4*(kt2/g_s));
+    return 0.5*g_s*(sqrt(dummy) -x1*x2);
+}
 
-    not_success = phasespace_integral(upper_limits, lower_limits, p_value, p_error);
-    return not_success;
+
+///u_hat
+///Calculates and returns the subprocess mandelstam variable u. Return value can be complex.
+///
+///\param p_x1 = pointer to momentum fraction of the first particle
+///\param p_x2 = pointer to momentum fraction of the other particle
+///\param p_kt2 = pointer to the transverse momentum squared
+///
+complex<double> u_hat(const double * const p_x1, const double * const p_x2, const double * const p_kt2)
+{
+    const double x1 = *p_x1, x2 = *p_x2, kt2 = *p_kt2;
+    const complex<double> dummy = x1*x2*(x1*x2 -4*(kt2/g_s));
+    return -0.5*g_s*(sqrt(dummy) +x1*x2);
+}
+
+
+///f_ses
+///Calculates and returns the parton distribution function F_ses according to single effective
+///subprocess approximation.
+///
+///\param p_x = pointer to momentum fraction of the particle in question
+///\param p_q2 = pointer to momentum exchange squared
+///\param p_pdf = pointer to a member of the PDF class from LHAPDF
+///
+double f_ses(const double * const p_x, const double * const p_q2, const PDF* p_pdf)
+{
+    double sum = 0;
+    for (int flavour=-6; flavour<=6; ++flavour)
+    {
+        sum += (flavour == 0) ? p_pdf->xfxQ2(flavour, *p_x, *p_q2) : 4 * p_pdf->xfxQ2(flavour, *p_x, *p_q2) / 9;
+    }
+    //cout<<"x="<<*p_x<<", q2="<<*p_q2<<" SES="<<sum<<endl;
+    return sum;
+}
+
+
+///sigma_gg_gg
+///Calculates and returns the cross section of the subprocess gg->gg. Return value should be real.
+///
+///\param p_s_hat = pointer to subprocess mandelstam variable s
+///\param p_t_hat = pointer to subprocess mandelstam variable t
+///\param p_u_hat = pointer to subprocess mandelstam variable u
+///
+complex<double> sigma_gg_gg(const complex<double> * const p_s_hat, const complex<double> * const p_t_hat, const complex<double> * const p_u_hat)
+{
+    const complex<double> s = *p_s_hat, t = *p_t_hat, u = *p_u_hat, constant = 3;
+    return 4.5*(constant-(u*t)/(s*s)-(u*s)/(t*t)-(s*t)/(u*u));
 }
